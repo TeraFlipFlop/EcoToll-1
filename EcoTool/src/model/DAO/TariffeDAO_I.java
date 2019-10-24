96e4e004695caa5b9ae4a5e8c6c1887513d5a31e
OwO <OwO@10.208.10.162>
2019-10-24 11:40:14 +0200

    waah

--- EcoTool/src/model/DAO/TariffeDAO_I.java
+++ EcoTool/src/model/DAO/TariffeDAO_I.java
@@ -3,7 +3,7 @@ package model.DAO;
 public interface TariffeDAO_I {
 	public double  get_tariffa(String classe, String tipo) ;
 	public double get_oneri(String ClasseAmb);
-	public void update_tariffe(double A,double B,double c3,double c4, double c5, String tipo );
-	public void update_oneri(double E1,double E2,double E3,double E4, double E5, double SE6);
+	public void update_tariffe(double []A, String tipo );
+	public void update_oneri(double [] E);
 
 }
